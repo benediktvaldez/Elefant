fd06d90d8b62b50977ec2abe633f121751d561d6
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-10-28 01:13:45 +0000

    Make logo clickable link to /

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 const React = require('react')
-const { Match, Miss, BrowserRouter } = require('react-router')
+const { BrowserRouter, Match, Miss, Link } = require('react-router')
 
 require('./index.css')
 
@@ -10,12 +10,20 @@ const Index = require('../_views/Index')
 const Disgraced = require('../_views/Disgraced')
 const Error404 = require('../_views/Error404')
 
+const LogoLink = () => {
+  return (
+    <Link to="/">
+      <Logo letters={location.pathname === '/' ? true : false} />
+    </Link>
+  )
+}
+
 const App = () => {
   return (
     <BrowserRouter>
       <div className="App">
         <BackgroundImages />
-        <Match pattern="/" render={({ location }) => <Logo letters={location.pathname === '/' ? true : false} />} />
+        <Match pattern="/" render={({ location }) => <LogoLink />} />
         <Match exactly pattern="/" component={Index} />
         <Match pattern="/disgraced" component={Disgraced} />
         <Miss component={Error404} />
