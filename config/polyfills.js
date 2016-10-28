3709d9b35a28b6763246332965cda1ffc655c26e
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-10-28 00:07:01 +0000

    Implement normalize.css

--- config/polyfills.js
+++ config/polyfills.js
@@ -9,6 +9,8 @@ if (typeof Promise === 'undefined') {
 // fetch() polyfill for making API calls.
 require('whatwg-fetch');
 
+require('normalize.css');
+
 // Object.assign() is commonly used with React.
 // It will use the native implementation if it's present and isn't buggy.
 Object.assign = require('object-assign');
