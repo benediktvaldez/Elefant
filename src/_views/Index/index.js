810081b53ac10a40ea10e896b73073f25c479149
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-10-28 01:15:32 +0000

    Basic styles + entry for index navigation

--- src/_views/Index/index.js
+++ src/_views/Index/index.js
@@ -1,18 +1,46 @@
 const React = require('react')
-// const { Link } = require('react-router')
+const { PropTypes } = React
+const { Link } = require('react-router')
+const cx = require('classnames')
 
 require('./index.css')
 
-const Index = () => {
-  return (
-    <div className="View Index">
-      {/*
-      <nav>
-        <Link to="/disgraced">Disgraced</Link>
-      </nav>
-      */}
-    </div>
-  )
+const Index = React.createClass({
+  getInitialState() {
+    return {
+      visible: false,
+    }
+  },
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        visible: true,
+      })
+    }, 200)
+  },
+
+  render() {
+    const className = cx([
+      'Navigation',
+      this.props.fadeIn ? (this.state.visible ? '' : 'hide') : ''
+    ])
+    return (
+      <div className="View Index">
+        <nav className={className}>
+          <Link to="/disgraced">sk&ouml;mm</Link>
+        </nav>
+      </div>
+    )
+  }
+})
+
+Index.propTypes = {
+  fadeIn: PropTypes.bool,
+}
+
+Index.defaultProps = {
+  fadeIn: true,
 }
 
 module.exports = Index
