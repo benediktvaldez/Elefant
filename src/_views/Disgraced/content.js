29f5a743f1b02ff6bac8ae8e818f86a0f045a225
Benedikt D Valdez <benediktvaldez@gmail.com>
2016-11-24 23:27:51 +0000

    WIP: Disgraced page

---
+++ src/_views/Disgraced/content.js
@@ -0,0 +1,24 @@
+const React = require('react')
+
+module.exports = {
+  '/disgraced/about': {
+    title: 'um sýninguna',
+    content: (
+      <div>
+        <p>Skömm segir frá lögfræðingi í New York að nafni Amir Kapoor sem er á hraðri leið upp metorðastigann. Hann er trúleysingi og hefur farið hljótt um Pakistanskan uppruna sinn, svo að fordómar og afstaða yfirmanna hans hindri ekki frama hans innan fyrirtækisins.</p>
+        <p>Eftir að hafa látið undan þrýstingi frá konu sinni Emilíu, sem er hvítur listmálari sem vinnur með symbolisma trúarbragða um að vera viðstaddur réttarhöld yfir múslimaleiðtoga fá yfirmenn hans efasemdir um að Amir sé ekki allur þar sem hann er séður.</p>
+        <p>Í verkinu heldur parið matarboð fyrir vinahjón sín Jórunni, þeldökk kristin kona og eiginmann hennar Ísak sem er gyðingur og eigandi listagallerís. Kvöldið fer frá rólindis spjalli við matarborðið yfir í snörp orðaskipti og fljótt koma í ljós leyndarmál sem sína að allt þetta fólk svífst einskis til að ná markmiðum sínum.</p>
+      </div>
+      ),
+  },
+  '/disgraced/group': {
+    title: 'hópurinn',
+    content: (
+    <div>
+      <p>Leikhópurinn er gríðarlega spenntur að fá að setja upp Skömm sem er eitt mest uppsetta verkið í heiminum í dag.</p>
+      <p>Skömm (e. Disgraced) vann hin virtu <a href="http://www.pulitzer.org/prize-winners-by-category/218" target="_blank">Pulitzer</a> verðlaun árið 2013 ásamt því að vera tilnefnt sem besta leikritið á Tony verðlaunahátíðinni 2015. Verkið fjallar á dramatískan hátt um þær sögur sem við segjum vinum okkar, leyndarmálunum sem við deilum með mökum okkar og lygunum sem við segjum sjálfum okkur til þess að fitta inn í samfélagið.</p>
+      <p>Höfundur verksins, <a href="http://ayadakhtar.com/" target="_blank">Ayad Akhtar</a>, hefur vakið mikla athygli fyrir skrif sín en hann er einkar lunkinn við að finna þessa dramatísku tengingu við verkið sem áhorfendur kalla eftir.  Skömm þorir að takast á við sannleikann sem leynist oft undir yfirborðinu.</p>
+    </div>
+    ),
+  },
+}
--- src/_views/Disgraced/index.js
+++ src/_views/Disgraced/index.js
@@ -1,11 +1,82 @@
 const React = require('react')
+const { PropTypes } = React
+const { Match, Link } = require('react-router')
+const cx = require('classnames')
 
-const Disgraced = () => {
+const src = require('./content')
+require('./index.css')
+
+const Content = ({ location })  => {
+  const { title, content } = src[location.pathname]
   return (
-    <div className="View Disgraced">
-      <p>Disgraced</p>
+    <div className="">
+      <h2>{title}</h2>
+      {content}
     </div>
   )
+}
+
+const Disgraced = React.createClass({
+  getInitialState() {
+    return {
+      visible: false,
+    }
+  },
+
+  show() {
+    this.setState({
+      visible: true,
+    })
+  },
+
+  hide() {
+    this.setState({
+      visible: false,
+    })
+  },
+
+  componentDidMount() {
+    console.log('mount')
+    setTimeout(() => {
+      this.show()
+    }, 200)
+  },
+
+  componentWillReceiveProps(nextProps) {
+    console.log('componentWillUpdate', nextProps.location.pathname)
+    this.hide()
+  },
+
+  render() {
+    const className = cx([
+      'View',
+      'Disgraced',
+      (this.props.location.pathname === '/disgraced' ? 'index' : 'sub'),
+    ])
+    return (
+      <div className={className}>
+        <hgroup className="titleNav">
+          <h1><Link to="/disgraced" className="title">sk&ouml;mm.</Link></h1>
+          <nav className="SubNavigation">
+            <Link to="/disgraced/about" className={location.pathname === '/disgraced/about' ? 'active' : ''}>um sýninguna</Link>
+            <Link to="/disgraced/group" className={location.pathname === '/disgraced/group' ? 'active' : ''}>hópurinn</Link>
+          </nav>
+        </hgroup>
+        <section className="Content">
+          <Match pattern="/disgraced/about" render={({ location }) => <Content location={location} />} />
+          <Match pattern="/disgraced/group" render={({ location }) => <Content location={location} />} />
+        </section>
+      </div>
+    )
+  }
+})
+
+Disgraced.propTypes = {
+  fadeIn: PropTypes.bool,
+}
+
+Disgraced.defaultProps = {
+  fadeIn: true,
 }
 
 module.exports = Disgraced
